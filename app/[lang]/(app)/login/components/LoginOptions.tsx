6544e0b3be134f500bc340d85fecce6b3f0038a0
hrawebdev <hrawebdev@gmail.com>
2025-12-24 10:42:56 +0330

    add well known route and update login options

--- app/[lang]/(app)/login/components/LoginOptions.tsx
+++ app/[lang]/(app)/login/components/LoginOptions.tsx
@@ -1,13 +1,22 @@
+'use client';
 import { type LoginDictionary } from '@/internalization/app/dictionaries/login/dictionary';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import { useLoginContext } from '../services/login/loginContext';
 
 export default function LoginOptions({ dic }: { dic: LoginDictionary }) {
+ const { loginModalIsOpen } = useLoginContext();
  return (
-  <div className='mt-1'>
-   <Button variant='link' asChild className='px-0 text-xs'>
-    <Link href='#'>{dic.forgetPassword}</Link>
-   </Button>
-  </div>
+  <>
+   {loginModalIsOpen ? (
+    <></>
+   ) : (
+    <div className='mt-1'>
+     <Button variant='link' asChild className='px-0 text-xs'>
+      <Link href='#'>{dic.forgetPassword}</Link>
+     </Button>
+    </div>
+   )}
+  </>
  );
 }
