e392fc6a124f566e89148e2bd24391c96f33ebb0
hrawebdev <hrawebdev@gmail.com>
2025-12-28 15:44:02 +0330

    update onChange user id

--- app/[lang]/(app)/(panel)/general-settings/users/components/users-list/UserStatusSwitch.tsx
+++ app/[lang]/(app)/(panel)/general-settings/users/components/users-list/UserStatusSwitch.tsx
@@ -5,8 +5,12 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { toast } from 'sonner';
 import { Spinner } from '@/components/ui/spinner';
+import { useUsersConfigContext } from '../../services/usersConfigContext';
 
 export default function UserStatusSwitch({ user }: { user: User }) {
+ const {
+  users: { onChangeSelectedUserID },
+ } = useUsersConfigContext();
  const queryClient = useQueryClient();
  const { mutate, isPending } = useMutation({
   mutationFn() {
@@ -19,6 +23,7 @@ export default function UserStatusSwitch({ user }: { user: User }) {
    queryClient.invalidateQueries({
     queryKey: [usersBasePath, 'user', user.personID.toString()],
    });
+   onChangeSelectedUserID(null);
   },
   onError(err: AxiosError<string>) {
    toast.error(err.message);
