f4a7f196a92c643f0d4978e3f760bf4d8d17c1dc
hrawebdev <hrawebdev@gmail.com>
2026-01-02 15:02:51 +0330

    add companies finder

---
+++ app/[lang]/(app)/(panel)/general-settings/companies/components/companies-finder/CompaniesFinder.tsx
@@ -0,0 +1,40 @@
+import { type CompaniesDictionary } from '@/internalization/app/dictionaries/general-settings/companies/dictionary';
+import PersonsWrapper from '../PersonsWrapper';
+import {
+ Dialog,
+ DialogContent,
+ DialogHeader,
+ DialogTitle,
+ DialogFooter,
+} from '@/components/ui/dialog';
+import { WrapperTypes } from '../../utils/wrapperTypes';
+
+export default function CompaniesFinder({
+ dic,
+ open,
+ onOpenChange,
+ wrapperType,
+}: {
+ open: boolean;
+ dic: CompaniesDictionary;
+ onOpenChange: (open?: boolean) => unknown;
+ wrapperType: Omit<Extract<WrapperTypes, { mode: 'find' }>, 'mode'>;
+}) {
+ return (
+  <Dialog open={open} onOpenChange={onOpenChange}>
+   <DialogContent className='flex flex-col w-full h-full sm:w-[min(95%,70rem)] sm:max-h-[95svh] max-w-none! p-0 overflow-hidden gap-0'>
+    <DialogHeader className='shrink-0 p-5 border-b border-input'>
+     <DialogTitle>{dic.title}</DialogTitle>
+    </DialogHeader>
+    <PersonsWrapper
+     dic={dic}
+     wrapperType={{
+      mode: 'find',
+      ...wrapperType,
+     }}
+    />
+    <DialogFooter></DialogFooter>
+   </DialogContent>
+  </Dialog>
+ );
+}
