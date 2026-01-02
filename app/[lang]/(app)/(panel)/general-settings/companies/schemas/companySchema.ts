f596407aba271e52c9b6518fe53074be20589ac9
hrawebdev <hrawebdev@gmail.com>
2026-01-02 10:24:15 +0330

    update companies schema

--- app/[lang]/(app)/(panel)/general-settings/companies/schemas/companySchema.ts
+++ app/[lang]/(app)/(panel)/general-settings/companies/schemas/companySchema.ts
@@ -1,11 +1,34 @@
 import { z } from 'zod';
 import { type CompaniesDictionary } from '@/internalization/app/dictionaries/general-settings/companies/dictionary';
 
-const defaultValues: Partial<CompanySchema> = {};
+const defaultValues: Partial<CompanySchema> = {
+ name: '',
+ nationalCode: '',
+ postalCode: '',
+ fax: '',
+ tel1: '',
+ tel2: '',
+ tel3: '',
+ nationality: null,
+ address: '',
+};
 
 function createCompanySchema({}: { dic: CompaniesDictionary }) {
  return z.object({
   name: z.string(),
+  nationalCode: z.string(),
+  postalCode: z.string(),
+  fax: z.string(),
+  tel1: z.string(),
+  tel2: z.string(),
+  tel3: z.string(),
+  address: z.string(),
+  nationality: z
+   .object({
+    key: z.string(),
+    value: z.string(),
+   })
+   .nullable(),
  });
 }
 
