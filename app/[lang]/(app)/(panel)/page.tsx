29068642e73a0d256ed8f0e6dc5626605330425c
hrawebdev <hrawebdev@gmail.com>
2026-01-30 14:27:18 +0330

    add home page logo

--- app/[lang]/(app)/(panel)/page.tsx
+++ app/[lang]/(app)/(panel)/page.tsx
@@ -1,3 +1,20 @@
 export default function Panel() {
- return <div></div>;
+ return (
+  <div className='h-full grid place-content-center'>
+   <div className='w-160'>
+    <img
+     className='dark:hidden'
+     alt='alin logo'
+     src='/images/logo/alincloud-light.png'
+     draggable={false}
+    />
+    <img
+     className='hidden dark:block'
+     alt='alin logo'
+     src='/images/logo/alincloud-dark.png'
+     draggable={false}
+    />
+   </div>
+  </div>
+ );
 }
